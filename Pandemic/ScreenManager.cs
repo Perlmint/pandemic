c5488090644b16157fa73304909c53782834ecff
omniavinco <omniavinco@gmail.com>
2012-08-11 02:55:03 +0900

    ...

---
+++ Pandemic/ScreenManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pandemic
+{
+    class ScreenManager
+    {
+        Vector2 screenCenter;
+        float scale;
+
+        public ScreenManager()
+        {
+            screenCenter = new Vector2(0, 0);
+            scale = 1;
+        }
+
+        protected Vector2 translateWorldToScreen(Vector2 worldCoord)
+        {
+            Vector2 result = (worldCoord - screenCenter) * scale;
+            return result;
+        }
+
+        protected Vector2 translateScreenToWorld(Vector2 screenCoord)
+        {
+            Vector2 result = (screenCenter - screenCoord) / scale;
+            return result;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameObject []drawableObjects)
+        {
+            foreach (GameObject drawableObject in drawableObjects)
+            {
+                Vector2 screenCoord;
+                //spriteBatch.Draw(translateWorldToScreen(drawableObject));
+            }
+        }
+    }
+}
